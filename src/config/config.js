102d6fb20810422baf83fdf7eb46cfaa833b8c14
PontusBengtsson <pontus.bengtsson@live.com>
2025-01-30 23:00:31 +0100

    Netlify 3

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 export const clientId = import.meta.env.VITE_CLIENT_ID;
 export const liveURL = import.meta.env.VITE_CLIENT_SECRET;
-export const devURL = 'http://localhost:5173';
+export const devURL = 'https://localhost:5173';
 export const redirectURL = import.meta.env.PROD ? liveURL : devURL;
 export const scopes = [
 	'playlist-read-collaborative',
